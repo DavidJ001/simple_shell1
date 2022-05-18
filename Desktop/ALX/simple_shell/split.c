37b4af23580531956bfed374c7dcc498075a8db1
DavidJ001 <Davidjoel392@gmail.com>
2022-05-19 00:27:23 +0300

    final trial

--- Desktop/ALX/simple_shell/split.c
+++ Desktop/ALX/simple_shell/split.c
@@ -1,17 +1,16 @@
 #include "shell.h"
-
 int token_len(char *str, char *delim);
 int count_tokens(char *str, char *delim);
 char **_strtok(char *line, char *delim);
 
 /**
  * token_len - Locates the delimiter index marking the end
- *             of the first token contained within a string.
- * @str: The string to be searched.
- * @delim: The delimiter character.
+ *             of the first token contained within a string
+ * @str: The string to be searched
+ * @delim: The delimiter character
  *
  * Return: The delimiter index marking the end of
- *         the intitial token pointed to be str.
+ *         the intitial token pointed to be str
  */
 int token_len(char *str, char *delim)
 {

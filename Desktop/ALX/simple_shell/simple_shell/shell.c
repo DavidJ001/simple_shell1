93f10f1da9983092977da5d28543c7f18bc7edf2
DavidJ001 <Davidjoel392@gmail.com>
2022-05-18 16:18:06 +0300

    new commits

--- Desktop/ALX/simple_shell/simple_shell/shell.c
+++ Desktop/ALX/simple_shell/simple_shell/shell.c
@@ -1,95 +1,40 @@
-#include "shell.h"
+#include "main.h"
+
 /**
- * read_file - Read Command From File
- * @filename:Filename
- * @argv:Program Name
- * Return: -1 or  0
+ * main - interactive command line interpreter
+ * Return: Zero if successful.
  */
 
-void read_file(char *filename, char **argv)
+int main(void)
 {
-	FILE *fp;
-	char *line = NULL;
-	size_t len = 0;
-	int counter = 0;
+	char *string, **tokens;
+	size_t n = 0;
+	ssize_t gl, fd = 0;
+	int status = 0, tty = 1, check_b_ins, error = 0;
 
-	fp = fopen(filename, "r");
-	if (fp == NULL)
-	{
-		exit(EXIT_FAILURE);
-	}
-	while ((getline(&line, &len, fp)) != -1)
-	{
-		counter++;
-		treat_file(line, counter, fp, argv);
-
-	}
-	if (line)
-		free(line);
-	fclose(fp);
-	exit(0);
-}
-/**
- * treat_file - PARSE Check Command Fork Wait Excute in Line of File
- * @line: Line From A File
- * @counter:Error Counter
- * @fp:File Descriptor
- * @argv:Program Name
- * Return : Excute A line void
- */
-void treat_file(char *line, int counter, FILE *fp, char **argv)
-{
-	char **cmd;
-	int st = 0;
-
-	cmd = parse_cmd(line);
-
-		if (_strncmp(cmd[0], "exit", 4) == 0)
-		{
-			exit_bul_for_file(cmd, line, fp);
-		}
-		else if (check_builtin(cmd) == 0)
-		{
-			st = handle_builtin(cmd, st);
-			free(cmd);
-		}
+	isatty(STDIN_FILENO) == 0 ? tty = 0 : tty;
+	do {
+		string = NULL, tokens = NULL, n = 0, status = 0;
+		tty == 1 ? fd = write(STDOUT_FILENO, "($) ", 4) : tty;
+		if (fd == -1)
+			dprintf(STDERR_FILENO, "Can't write the stdout");
+		gl = getline(&string, &n, stdin);
+		if (gl == -1)
+		{	free(string);
+			if (feof(stdin))
+				return (error);
+			else
+				return (EXIT_FAILURE); }
+		tokens = _strtok(tokens, string, " \t\n\"\'");
+		check_b_ins = check_builtins(tokens, string, &error);
+		if (check_b_ins == 1)
+			status = 1;
+		if (status == 0)
+			check_execution(tokens, string, &error);
 		else
-		{
-			st = check_cmd(cmd, line, counter, argv);
-			free(cmd);
-		}
-}
-/**
- * exit_bul_for_file - Exit Shell Case Of File
- * @line: Line From A File
- * @cmd: Parsed Command
- * @fd:File Descriptor
- * Return : Case Of Exit in A File Line
- */
-void exit_bul_for_file(char **cmd, char *line, FILE *fd)
-{
-	int statue, i = 0;
-
-	if (cmd[1] == NULL)
-	{
-		free(line);
-		free(cmd);
-		fclose(fd);
-		exit(errno);
-	}
-	while (cmd[1][i])
-	{
-		if (_isalpha(cmd[1][i++]) < 0)
-		{
-			perror("illegal number");
-		}
-	}
-	statue = _atoi(cmd[1]);
-	free(line);
-	free(cmd);
-	fclose(fd);
-	exit(statue);
-
-
-
+			free(tokens);
+		free(string);
+	} while (1);
+	free(tokens);
+	return (0);
 }
